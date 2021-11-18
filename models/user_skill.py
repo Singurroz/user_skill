94de35e7cdd8826cc59c8c9a834b11e096b670a2
Emmanuel Cruz <singurr852@gmail.com>
2021-11-18 02:16:27 -0600

    Wizard para importacion CSV

--- models/user_skill.py
+++ models/user_skill.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
+import base64
+import io
 
-from odoo import models, fields, api
+from odoo import models, fields, exceptions
 
 
 class UserSkill(models.Model):
@@ -25,3 +27,37 @@ class UserSkill(models.Model):
 #     def _value_pc(self):
 #         for record in self:
 #             record.value2 = float(record.value) / 100
+
+
+class UploadFile(models.TransientModel):
+    _name = 'user_skill.upload_file'
+
+    upload_file = fields.Binary(string='Subir fichero', required=True)
+    file_name = fields.Char(string='Nombre del fichero')
+
+    def import_file(self):
+        if self.file_name:
+            if '.csv' not in self.file_name:
+                raise exceptions.ValidationError('El archivo debe ser un CSV')
+            file = self.read_file_from_binary(self.upload_file)
+            lines = file.split('\n')
+            for line in lines:
+                elements = line.split(';')
+                if len(elements) > 1:
+                    self.env['user_skill.user_skill'].create({
+                        'partner_id': elements[0],
+                        'name': elements[1],
+                        'skill': elements[2],
+                        'years': 0,
+                        'percent': 0.0,
+                        'company_id': elements[5],
+                    })
+
+    def read_file_from_binary(self, file):
+        try:
+            with io.BytesIO(base64.b64decode(file)) as f:
+                f.seek(0)
+                return f.read().decode('UTF-8')
+        except Exception as e:
+            print(str(e))
+            raise e
